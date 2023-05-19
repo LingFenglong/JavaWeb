c890672276333c9848b9f35a2ff675079da21175
LingFenglong <2808021998@qq.com>
2023-05-19 23:51:02 +0800

    Update VerificationCode

---
+++ VerificationCode/src/main/java/com/servlets/ShowStatusServlet.java
@@ -0,0 +1,53 @@
+package com.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class ShowStatusServlet
+ */
+@WebServlet("/showstatus")
+public class ShowStatusServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowStatusServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		response.setHeader("refresh", "0.5; login");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter writer = response.getWriter();
+		String state = (String) session.getAttribute("state");
+
+		writer.write("<html><body>");
+		writer.write("<h1>" + state + "</h1>");
+		writer.write("</body></html>");
+		
+		writer.flush();
+		writer.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
